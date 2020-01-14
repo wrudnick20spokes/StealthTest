29a33518dbd5d7d9e3ddc83dfac2592da0f31871
William Rudnick <williamrudnick@Williams-MacBook-Pro-2.local>
2020-01-14 08:48:11 -0600

    Cleanup

--- bot/controllers/bot_controller.rb
+++ bot/controllers/bot_controller.rb
@@ -3,7 +3,7 @@ class BotController < Stealth::Controller
   helper :all
 
   def route
-    if current_session.present? and current_session.state_string != 'say_goodbye'
+    if current_session.present
       step_to session: current_session
     else
       step_to flow: 'hello', state: 'say_hello'
