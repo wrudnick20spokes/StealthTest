455f8dedf63c3c69ae111e4415000f0337f77561
William Rudnick <williamrudnick@Williams-MacBook-Pro-2.local>
2020-01-14 12:34:06 -0600

    Adding way to restart

--- bot/controllers/bot_controller.rb
+++ bot/controllers/bot_controller.rb
@@ -3,7 +3,7 @@ class BotController < Stealth::Controller
   helper :all
 
   def route
-    if current_session.present?
+    if current_session.present? and current_message.message != 'restart'
       step_to session: current_session
     else
       step_to flow: 'hello', state: 'say_hello'
