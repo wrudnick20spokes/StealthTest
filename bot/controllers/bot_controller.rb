4ec768b8512b068eb382bcc974f9b2262b0a7b2b
William Rudnick <williamrudnick@Williams-MacBook-Pro-2.local>
2020-01-16 13:17:57 -0600

    Prepping to save and resume sessions

--- bot/controllers/bot_controller.rb
+++ bot/controllers/bot_controller.rb
@@ -3,9 +3,7 @@ class BotController < Stealth::Controller
   helper :all
 
   def route
-    if current_message.message == "ENTERING_APPLICATION"
-      step_to flow: 'hello', state: 'say_hello'
-    elsif current_session.present?
+    if current_message.message == "ENTERING_APPLICATION" or current_session.present?
       step_to session: current_session
     else
       step_to flow: 'hello', state: 'say_hello'
