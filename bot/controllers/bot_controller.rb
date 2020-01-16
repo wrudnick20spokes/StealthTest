5c493c2fcbdaf582565a32b424db04c2973b3294
William Rudnick <williamrudnick@Williams-MacBook-Pro-2.local>
2020-01-16 13:25:22 -0600

    Changing logic in bot controller to route to session or hello state

--- bot/controllers/bot_controller.rb
+++ bot/controllers/bot_controller.rb
@@ -3,10 +3,12 @@ class BotController < Stealth::Controller
   helper :all
 
   def route
-    if current_message.message == "ENTERING_APPLICATION" or current_session.present?
+    if current_message.message == "ENTERING_APPLICATION" and current_session.present?
+      update_session_to session: current_session
+    elsif current_session.present?
       step_to session: current_session
     else
-      step_to flow: 'hello', state: 'say_hello'
+      update_session_to flow: 'hello', state: 'say_hello'
     end
   end
 
