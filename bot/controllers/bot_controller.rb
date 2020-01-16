08434f534b7b346337406b5e8fb9a544e143bc7e
William Rudnick <williamrudnick@Williams-MacBook-Pro-2.local>
2020-01-16 14:24:46 -0600

    Changing logic to push people back a state when they reenter the application

--- bot/controllers/bot_controller.rb
+++ bot/controllers/bot_controller.rb
@@ -3,12 +3,14 @@ class BotController < Stealth::Controller
   helper :all
 
   def route
-    if current_message.message == "ENTERING_APPLICATION" and current_session.present?
-      update_session_to session: current_session
-    elsif current_session.present?
-      step_to session: current_session
+    if current_message.message == 'ENTERING_APPLICATION' and current_session.present?
+      step_to session: current_session - 1.states
     else
-      step_to flow: 'hello', state: 'say_hello'
+      if current_session.present?
+        step_to session: current_session
+      else
+        step_to flow: 'hello', state: 'say_hello'
+      end
     end
   end
 
