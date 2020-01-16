cb6b6c2955350155d565113562c79ae482aff4f1
William Rudnick <williamrudnick@Williams-MacBook-Pro-2.local>
2020-01-16 09:23:58 -0600

    Bumping stealth renalis version

--- bot/controllers/bot_controller.rb
+++ bot/controllers/bot_controller.rb
@@ -3,9 +3,6 @@ class BotController < Stealth::Controller
   helper :all
 
   def route
-    puts "START INCOMING MESSAGE"
-    puts current_message.message
-    puts "END INCOMING MESSAGE"
     if current_message.message == "ENTERING_APPLICATION"
       step_to flow: 'hello', state: 'say_hello'
     elsif current_session.present?
