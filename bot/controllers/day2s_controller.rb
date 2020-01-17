0241f7c188cfe3d2ba1d364079f2bcab495a2925
William Rudnick <williamrudnick@Williams-MacBook-Pro-2.local>
2020-01-17 09:25:28 -0600

    Putting state names in quotes

--- bot/controllers/day2s_controller.rb
+++ bot/controllers/day2s_controller.rb
@@ -3,21 +3,21 @@ class Day2sController < BotController
   # INTRO
   def say_hello
     send_replies
-    update_session_to state: get_hello_response
+    update_session_to state: 'get_hello_response'
   end
 
   def get_hello_response
-    step_to state: say_intro_1
+    step_to state: 'say_intro_1'
   end
 
   def say_intro_1
     send_replies
-    update_session_to state: get_intro_1_response
+    update_session_to state: 'get_intro_1_response'
   end
 
   def get_intro_1_response
     puts current_message.message
-    step_to state: say_intro_2
+    step_to state: 'say_intro_2'
   end
 
 end
