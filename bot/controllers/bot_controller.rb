ffdae2a9177affa9ff79244b8bab7cbf1f040ad1
William Rudnick <williamrudnick@Williams-MacBook-Pro-2.local>
2020-01-20 16:24:06 -0600

    New possible way to force application to a certain day

--- bot/controllers/bot_controller.rb
+++ bot/controllers/bot_controller.rb
@@ -3,7 +3,10 @@ class BotController < Stealth::Controller
   helper :all
 
   def route
-    if current_message.message.include? "STARTING_DAY"
+    if current_message.message.include? "FORCING_DAY"
+      day_number = current_message.message.partition(':').last
+      step_to flow: "day#{day_number}", state: "say_hello"
+    elsif current_message.message.include? "STARTING_DAY"
       day_number = current_message.message.partition(':').last
       step_to flow: "day#{day_number}", state: "say_hello"
     elsif current_message.message.include? "RESUMING_DAY" and current_session.present?
