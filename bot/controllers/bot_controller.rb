41fa72cfbef5fc89a74150944d511811eda489e0
William Rudnick <williamrudnick@Williams-MacBook-Pro-2.local>
2020-01-20 08:46:31 -0600

    Remving checkpoints making the app flow normally

--- bot/controllers/bot_controller.rb
+++ bot/controllers/bot_controller.rb
@@ -3,27 +3,25 @@ class BotController < Stealth::Controller
   helper :all
 
   def route
-    if current_message.message == 'ENTERING_APPLICATION'
-      step_to flow: 'day2', state: 'say_quiz_11'
+    # if current_message.message == 'ENTERING_APPLICATION'
+    #   step_to flow: 'day2', state: 'say_quiz_11'
+    # else
+    #   if current_session.present?
+    #     step_to session: current_session
+    #   else
+    #     step_to flow: 'day2', state: 'say_quiz_11'
+    #   end
+    # end
+
+    if current_message.message == 'ENTERING_APPLICATION' and current_session.present?
+      step_to session: current_session - 1.states
     else
       if current_session.present?
         step_to session: current_session
       else
-        step_to flow: 'day2', state: 'say_quiz_11'
+        step_to flow: 'day2', state: 'say_hello'
       end
     end
-
-    # if current_message.message == 'ENTERING_APPLICATION' and current_session.present?
-    #   step_to session: current_session - 1.states
-    # else
-    #   if current_session.present?
-    #     puts "SESSION PRESENT"
-    #     step_to session: current_session
-    #   else
-    #     puts "SESSION PRESENT"
-    #     step_to flow: 'day2', state: 'say_hello'
-    #   end
-    # end
   end
 
 end
