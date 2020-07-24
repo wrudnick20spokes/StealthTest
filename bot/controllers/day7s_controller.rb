bc4c516d56f178c486705ac2e99b45a4d4ef7f5d
William Rudnick <williamrudnick@Williams-MacBook-Pro-2.local>
2020-07-24 13:08:42 -0500

    One bug causing issues investigating now

--- bot/controllers/day7s_controller.rb
+++ bot/controllers/day7s_controller.rb
@@ -1,17 +1,7 @@
 class Day7sController < BotController
 
-  def method_missing(m, *args, &block)
-    if m.to_s.starts_with?("say_")
-      send_replies
-      update_session_to state: Day7Flow::DAY_7_FLOWS[m][:next]
-    elsif m.to_s.starts_with?("get_")
-      next_state = Day7Flow::DAY_7_FLOWS[m][:next]
-      if next_state.is_a? String
-        step_to state: next_state
-      else
-        step_to state: next_state[current_message.message]
-      end
-    end
+  def flow_map
+    Day7Flow::DAY_7_FLOWS
   end
 
   def say_goodbye
